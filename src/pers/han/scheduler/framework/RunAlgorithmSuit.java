577cdf97414cc113db0202b4fe8d23d10e99a1ea
hanYG <1501106169@qq.com>
2022-09-30 10:10:07 +0800

    add RunAlgorithmSuit.java

---
+++ src/pers/han/scheduler/framework/RunAlgorithmSuit.java
@@ -0,0 +1,14 @@
+package pers.han.scheduler.framework;
+
+/**
+ * 在多个调度任务上执行调度算法，RunAlgorithmCase的合集
+ * 
+ * @author		hanYG
+ * @createDate	2022年9月30日
+ * @alterDate	2022年9月30日
+ * @version		1.0
+ *
+ */
+public class RunAlgorithmSuit {
+
+}
