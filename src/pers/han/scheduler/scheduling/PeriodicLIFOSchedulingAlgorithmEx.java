72cbd181b587c2b672d4ee1524ac8394ebf8c8f4
hanYG <1501106169@qq.com>
2022-11-09 16:29:58 +0800

    add some scheduling algorithms

---
+++ src/pers/han/scheduler/scheduling/PeriodicLIFOSchedulingAlgorithmEx.java
@@ -0,0 +1,90 @@
+package pers.han.scheduler.scheduling;
+
+import pers.han.scheduler.task.PeriodicTask;
+import pers.han.scheduler.task.TimeBlock;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Vector;
+
+public class PeriodicLIFOSchedulingAlgorithmEx extends SchedulingAlgorithm {
+    class MyPeriodicTask extends PeriodicTask {
+        private Integer id;
+
+        MyPeriodicTask(PeriodicTask periodicTask, Integer taskId) {
+            super(periodicTask);
+            id = taskId;
+        }
+
+        MyPeriodicTask(PeriodicTask periodicTask, Integer taskId, Integer cycleStartTime) {
+            super(periodicTask);
+            id = taskId;
+            super.cycleStartTime = cycleStartTime;
+        }
+
+    }
+
+    class ExecuteInfo {
+
+        private Integer executeTime;
+        private Integer executedTaskId;
+
+        ExecuteInfo(Integer executedTaskId, Integer executeTime) {
+            this.executeTime = executeTime;
+            this.executedTaskId = executedTaskId;
+        }
+
+    }
+
+    private Vector<MyPeriodicTask> waitingTaskSet = new Vector<>();
+    private Stack<MyPeriodicTask> readyTaskSet = new Stack<>();
+    @Override
+    public Vector<TimeBlock> doSchedule() {
+
+
+        for (int i = 0; i < this.taskSet.size(); i++) {
+            waitingTaskSet.add(new MyPeriodicTask((PeriodicTask) this.taskSet.get(i).clone(), i));
+        }
+        int i = 0;
+        while (this.timeAxis < this.runEndTime) {
+            Iterator<MyPeriodicTask> itr = waitingTaskSet.iterator();
+            while (itr.hasNext()) {
+                MyPeriodicTask waitingTask = itr.next();
+                if (timeAxis >= waitingTask.getCycleStartTime() + waitingTask.getJobReleaseTime()) {
+                    readyTaskSet.push(waitingTask);
+                    itr.remove();
+                }
+            }
+            ExecuteInfo executeInfo;
+            executeInfo = executeOneJob();
+            int execTime = executeInfo.executeTime;
+            int executedTaskId = executeInfo.executedTaskId;
+            if (execTime == 0) {
+                this.timeAxis++;
+            } else {
+                this.schedulingResult.add(new TimeBlock(executedTaskId, this.timeAxis, execTime));
+                i++;
+                this.timeAxis += execTime;
+            }
+        }
+
+        return this.schedulingResult;
+    }
+
+    private ExecuteInfo executeOneJob(){
+        MyPeriodicTask lastTaskExecuted;
+        if(readyTaskSet.isEmpty()){
+            return new ExecuteInfo(-1,0);
+        }
+        MyPeriodicTask taskToExecute = readyTaskSet.peek();
+        int executeTime = taskToExecute.getJobExecTime();
+        taskToExecute.nextCycle();
+        lastTaskExecuted = taskToExecute;
+        waitingTaskSet.add(new MyPeriodicTask(lastTaskExecuted,
+                lastTaskExecuted.id, lastTaskExecuted.getCycleStartTime()));
+        readyTaskSet.pop();
+        return new ExecuteInfo(lastTaskExecuted.id, executeTime);
+    }
+}
+
